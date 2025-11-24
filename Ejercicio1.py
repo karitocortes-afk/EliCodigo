b43ebb54de411c1c84c595da27d8b99e586dc413
karitocortes-afk <karitolcortes2008@gmail.com>
2025-11-24 17:33:43 -0500

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,31 @@
+#suma
+num = int(input("digite primer numero: "))
+num2 = int(input("digite segundo numero: ") )
+
+resultado = num + num2
+
+print("el resultado de la suma es: ", resultado)
+
+#resta
+
+resultado = num - num2
+
+print("el resultado de la resta es: ", resultado)
+
+#multiplicacion 
+
+resultado = num * num2
+
+print("el resultado de la multiplicaion es: ", resultado)
+
+#division 
+
+resultado = num / num2
+
+print("el resultado de la division es: ", resultado)
+ 
+#modelo 
+
+resultado = num % num2
+
+print("el resultado de la expocion es: ", resultado)
